480605329a767b9eb252795a1e1ed62b3f638611
Juan Bautista <jbautistapulido@gmail.com>
2025-01-07 18:09:20 -0500

    Working at the palette project

--- src/components/color-input/ColorInput.tsx
+++ src/components/color-input/ColorInput.tsx
@@ -3,20 +3,18 @@ import { useContext } from "react";
 function ColorInput() {
     return (
         <div className="flex flex-col items-center gap-2 md:flex-row">
-            <label htmlFor="" className="relative">
+            <label htmlFor="current-colors" className="relative">
                 <input
+                    aria-label="select a color"
                     className="absolute left-2 top-[6px]"
                     type="color"
-                    placeholder="#fff"
-                    value={"#fff"}
                 >
                 </input>
                 <input
+                    aria-label="type a color or observe the color selected"
                     className="py-[6px] pl-16 font-mono border-[1px] border-slate-700 rounded-[4px]"
+                    placeholder="#FDA12D"
                     type="text"
-                    name=""
-                    id=""
-                    placeholder="Zona de ingreso"
                 />
             </label>
         </div>
--- src/components/save-palette/SavePalette.tsx
+++ src/components/save-palette/SavePalette.tsx
@@ -1,7 +1,37 @@
+import { 
+  Tooltip,
+  TooltipContent,
+  TooltipProvider
+
+} from "@radix-ui/react-tooltip"
+import { TooltipTrigger } from "@radix-ui/react-tooltip";
+
+
+interface CopyPaletteProps{
+  colors: {color: string; text: string}[];
+
+}
 
 function SavePalette() {
   return (
-    <div>SavePalette</div>
+    <TooltipProvider>
+      <Tooltip delayDuration={200}>
+          <TooltipTrigger asChild>
+            <Button
+              aria-label="copy palette"
+              size={"sm"}
+              variant={"outline"}
+              onClick={handlerClick}
+              className="ml-2 rounded-[4px]"
+            >
+              <Copy></Copy>
+            </Button>
+          </TooltipTrigger>
+        <TooltipContent className="rounded-[4px]">
+          <p className="text-[12px]">Copy palette</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
 
