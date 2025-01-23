4f410bbf2b7e462958f8acc6fcc41789a3d63506
Juan Manuel Bautista <104174520+Bauti567@users.noreply.github.com>
2025-01-23 15:19:49 -0500

    Delete src directory

--- src/layouts/Layout.tsx
+++
@@ -1,7 +0,0 @@
-interface LayputProps{
-    children: React.ReactNode
-}
-
-export default function Layout({children}: LayputProps){
-    return <main className="max-w-screen-xl p-5 mx-auto">{children}</main>
-}
